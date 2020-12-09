a7dca9c576600739bd34d2c5677f6698534b8853
Ekaterina Gutorova <dredikyl@gmail.com>
2020-12-09 16:01:53 +0300

    JS_5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 
-/* eslint-disable default-case */
-/* eslint-disable no-unmodified-loop-condition */
-/* eslint-disable func-style */
-/* eslint-disable require-jsdoc */
-/* eslint-disable max-lines-per-function */
-/* eslint-disable max-statements */
-/* eslint-disable no-undef */
-
 let container = null;
 let prevIndicator = null;
 
@@ -140,4 +132,4 @@ function createCarousel(slidesCount = 5) {
     setListener();
 }
 
-createCarousel();
+createCarousel(4);
